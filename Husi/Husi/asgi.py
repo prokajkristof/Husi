fa60b7d0d888c9965f4fa034f0033d7efb4d7eaf
Szbzltn99 <szabozltn99@gmail.com>
2022-03-05 15:21:21 +0100

    Django starting files and directories

---
+++ Husi/Husi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Husi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Husi.settings')
+
+application = get_asgi_application()
