91488c191bf14d48dba0aaa817e12f5885a17a5e
prokajkristof <kiscoloshun@gmail.com>
2022-03-05 17:52:39 +0100

    feat(#1): Added Instructor class to model and to admin

--- elearning/admin.py
+++ elearning/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from elearning.models import Instructor
 
-# Register your models here.
+
+admin.site.register(Instructor)
+
--- elearning/models.py
+++ elearning/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.CharField(max_length=30, unique=True)
+    registration_date = models.DateField()
+    num_of_published_courses = models.IntegerField()
+    num_of_enrolled_students = models.IntegerField()
+    average_review_rating = models.FloatField()
+    num_of_reviews = models.IntegerField()
