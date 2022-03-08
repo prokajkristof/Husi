f40a566ebb20d3c91719a16f2f283dd345b01deb
prokajkristof <kiscoloshun@gmail.com>
2022-03-08 17:37:59 +0100

    feat(#1): Added Student class to model and to admin

--- elearning/admin.py
+++ elearning/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from elearning.models import Instructor
+from elearning.models import Instructor, Student
 
 
 admin.site.register(Instructor)
+admin.site.register(Student)
 
--- elearning/models.py
+++ elearning/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 
 class Instructor(models.Model):
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
-    email = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
     registration_date = models.DateField()
     num_of_published_courses = models.IntegerField()
     num_of_enrolled_students = models.IntegerField()
+    average_review_rating = models.FloatField()
+    num_of_reviews = models.IntegerField()
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=255, unique=True)
+    registration_date = models.DateField()
+    num_of_courses_enrolled = models.IntegerField()
+    num_of_courses_completed = models.IntegerField()
     average_review_rating = models.FloatField()
     num_of_reviews = models.IntegerField()
