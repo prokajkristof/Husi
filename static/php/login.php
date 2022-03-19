4d74b1642030be6656feb65ad2d92dddcd0a6047
BSX8GS <91091500+BSX8GS@users.noreply.github.com>
2022-03-19 18:48:39 +0100

    feat(#4): extended login.php
    I'm not sure how to connect to db, besides there are no password field in any of the tables.

--- static/php/login.php
+++ static/php/login.php
@@ -1,1 +1,21 @@
+<?php
+    //database connection
+    $user = //select count(*) from user where username = uname and password = pwd;
+    //No password in db :(
+?>
+<script>
+    var value = "<?php echo $user; ?>";
+    if(value == 0){
+        document.getElementsByName("login")[0].disabled = true;
+    }
 
+function validate(){
+if (value>0){
+alert ("Logged in successfully");
+window.location = "#"; // page to redirect
+}
+else{
+    alert("E-mail or password is incorrect");
+}
+}
+</script>
