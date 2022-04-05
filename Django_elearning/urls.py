f8660849de854bf6e90887eeb4541e2a40a931d3
BSX8GS <91091500+BSX8GS@users.noreply.github.com>
2022-04-05 21:20:22 +0200

    feat(#5): Added path to profile page

--- Django_elearning/urls.py
+++ Django_elearning/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from elearning.views import profile as p
 
 urlpatterns = [
     path('', include('elearning.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', p)
 ]
