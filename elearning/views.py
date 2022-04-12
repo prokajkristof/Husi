e1d0f88f51887b42cbd9d98a858cba504c187460
prokajkristof <kiscoloshun@gmail.com>
2022-04-12 18:38:31 +0200

    feat(#8): created the delete and create function for the Courses

--- elearning/views.py
+++ elearning/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Course
+from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.urls import reverse
 # Create your views here.
+
+
+from django.views.generic.edit import CreateView
+
+
+def delete(request, id):
+  course = Course.objects.get(id=id)
+  course.delete()
+  return HttpResponseRedirect(reverse('course-list'))
+
+
+class CourseCreateView(CreateView):
+    model = Course
+    fields = [
+        'course_title',
+        'course_brief',
+        'instructor_id',
+        'num_of_chapters'
+    ]
 
 
 class CourseListView(ListView):
