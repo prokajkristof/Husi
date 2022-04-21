a907252ad1ebae3072f9969d4ea1324141237a79
lacklack <molnar.lacko01@gmail.com>
2022-04-21 16:40:58 +0200

    Handling POST method in registerPage

--- elearning/views.py
+++ elearning/views.py
@@ -9,6 +9,11 @@ def index(request):
 
 def registerPage(request):
     form = UserCreationForm()
+
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
