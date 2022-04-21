4cefe1e4a6678575660080b972cec0daa7eaa260
lacklack <molnar.lacko01@gmail.com>
2022-04-21 18:55:16 +0200

    Redirecting from login.html to index.html

--- elearning/views.py
+++ elearning/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
 from django.contrib import messages
 
 # Create your views here.
@@ -24,5 +26,19 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+
+    if request.method =='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Hibás felhasználónév vagy jelszó!')
+
     context = {}
     return render(request, 'accounts/login.html', context)
+
