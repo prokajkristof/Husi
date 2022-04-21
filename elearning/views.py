fe5db59ad03d595ebb66eeec6b7f18a374d139e7
lacklack <molnar.lacko01@gmail.com>
2022-04-21 18:21:08 +0200

    Setting up links, and form error messages

--- elearning/views.py
+++ elearning/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
@@ -15,6 +15,7 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
