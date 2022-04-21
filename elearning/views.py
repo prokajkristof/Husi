a85a3248b246bb941bc4e76be8344cc000cfed6d
lacklack <molnar.lacko01@gmail.com>
2022-04-21 18:33:35 +0200

    Adding flash messages

--- elearning/views.py
+++ elearning/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib import messages
+
 # Create your views here.
 from .forms import CreateUserForm
 
@@ -15,6 +17,8 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.succes(request, 'Felhasználó sikeresen létrehozva (' + user + ')')
             return redirect('login')
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
