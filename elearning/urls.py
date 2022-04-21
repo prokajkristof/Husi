a8201c88cfefbae99c62122f1a43ef712105d8f7
lacklack <molnar.lacko01@gmail.com>
2022-04-21 16:19:54 +0200

    Adding register/login to views and urls Creating register.html

--- elearning/urls.py
+++ elearning/urls.py
@@ -4,5 +4,8 @@ from elearning import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+
 
 ]
--- elearning/views.py
+++ elearning/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'elearning/index.html')
+
+def registerPage(request):
+    context = {}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
