32aa7ab637f7dc48e317e9bc6fa2f9824aa424a6
lacklack <molnar.lacko01@gmail.com>
2022-04-21 17:27:54 +0200

    Adding email bar to form Editing forms.py and views.py

--- elearning/views.py
+++ elearning/views.py
@@ -3,15 +3,16 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from .forms import CreateUserForm
 
 def index(request):
     return render(request, 'elearning/index.html')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
 
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
     context = {'form':form}
