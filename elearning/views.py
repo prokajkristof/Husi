046ab7862275979162e4cfbc2e70522a588ec1df
lacklack <molnar.lacko01@gmail.com>
2022-04-21 16:32:34 +0200

    Importing Django user creation form

--- elearning/views.py
+++ elearning/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,7 +8,8 @@ def index(request):
     return render(request, 'elearning/index.html')
 
 def registerPage(request):
-    context = {}
+    form = UserCreationForm()
+    context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
