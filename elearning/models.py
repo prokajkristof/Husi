7f32f0174052c4e43d6d53d0dec1f370c87ade3f
Szbzltn99 <szabozltn99@gmail.com>
2022-04-26 19:25:58 +0200

    fix(#5): Changed the instructor and student table

--- elearning/models.py
+++ elearning/models.py
@@ -6,10 +6,6 @@ class Instructor(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50, unique=True)
     registration_date = models.DateField()
-    num_of_published_courses = models.IntegerField()
-    num_of_enrolled_students = models.IntegerField()
-    average_review_rating = models.FloatField()
-    num_of_reviews = models.IntegerField()
 
 
 class Student(models.Model):
@@ -17,8 +13,6 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=255, unique=True)
     registration_date = models.DateField()
-    num_of_courses_enrolled = models.IntegerField()
-    num_of_courses_completed = models.IntegerField()
 
 
 class Course(models.Model):
