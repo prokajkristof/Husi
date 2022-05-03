aa71d2bf9ada8156fe88a4fad98a7b34e19c5f28
prokajkristof <kiscoloshun@gmail.com>
2022-05-03 14:24:29 +0200

    feat(#17): added Course model test case

--- elearning/tests.py
+++ elearning/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from elearning.models import Course, Instructor, Student
+
+
+class InstructorTest(TestCase):
+    def test_fields(self):
+        instructor = Instructor()
+        instructor.first_name= "Test First"
+        instructor.last_name = "Test Last"
+        instructor.email = "Test Email"
+        instructor.registration_date = datetime.datetime.now()
+        instructor.num_of_published_courses = 1
+        instructor.num_of_enrolled_students = 1
+        instructor.average_review_rating = 3
+        instructor.num_of_reviews = 1
+        instructor.save()
+
+        record = Instructor.objects.get(id=1)
+        self.assertEqual(record, instructor)
+
+
